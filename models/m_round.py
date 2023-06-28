22e2e936c2e71b46d9e68bf79f43479cb3f2f92e
DayriseA_dom <adrien.maisak@gmail.com>
2023-06-28 08:50:28 +0200

    Method added in Round class to set-up matches

--- models/m_round.py
+++ models/m_round.py
@@ -17,3 +17,23 @@ class Round:
         self.start_date = datetime.datetime.now()
         self.end_date = None
         self.matches = matches or []
+
+    def setup_matches(self, participants: list[Player]) -> None:
+        """Set up the matches of the round by pairing participants."""
+        if len(participants) % 2 != 0:
+            for i in range(0, len(participants) - 1, 2):
+                self.matches.append(Match(participants[i], participants[i + 1]))
+            participants_exclude_last = participants[:-1]
+            random_participant = random.choice(participants_exclude_last)
+            self.matches.append(Match(random_participant, participants[-1]))
+
+        elif len(participants) % 2 == 0:
+            for i in range(0, len(participants), 2):
+                self.matches.append(Match(participants[i], participants[i + 1]))
+
+        print(f"{self.name} matches are set up:\n")
+        for match in self.matches:
+            player1_name = match.side1[0].firstname + " " + match.side1[0].lastname
+            player2_name = match.side2[0].firstname + " " + match.side2[0].lastname
+            color = match.player1_color
+            print(f"{player1_name} (in {color} VS {player2_name}\n")
